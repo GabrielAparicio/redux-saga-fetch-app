50fe7af5a581987253f2ee7eada042e87e6b93f5
GabrielAparicio <sgaparicio22@gmail.com>
2020-09-28 10:50:20 -0500

    Add store for state management

---
+++ src/store/store.tsx
@@ -0,0 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit';
+import usersSlice from './usersSlice';
+import sortingSlice from './sortingSlice';
+import filterSlice from './filterSlice';
+import createSagaMiddleware from 'redux-saga';
+import fetchUsersSaga from './fetchUsersSaga';
+import { combineReducers } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({
+    usersSlice, 
+    sortingSlice,
+    filterSlice
+});
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: [sagaMiddleware]
+});
+
+sagaMiddleware.run(fetchUsersSaga);
+
+export default store;
