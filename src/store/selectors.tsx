0cbc6ccb0dfd48a589b73ab1ddbbbccf88b19266
GabrielAparicio <sgaparicio22@gmail.com>
2020-09-28 12:20:20 -0500

    Setting general users selector

--- src/store/selectors.tsx
+++ src/store/selectors.tsx
@@ -15,8 +15,18 @@ export const usersSelector = createSelector(
     [users, sortingField, sortingType, filterField, filterContent],
     (users, sortingField, sortingType, filterField, filterContent) => {
         const filteredUsers = filterContent ? (
-            users.filter((user) => (user as any)[filterField].toLowerCase().includes(filterContent) )
+            users.filter((user) => user[filterField].toLowerCase().includes(filterContent) )
         ) : users;
         return _.orderBy(filteredUsers, [sortingField], [sortingType]);
     }
+);
+
+export const isLoadingSelector = createSelector(
+    [isLoading],
+    (isLoading) => isLoading
+);
+
+export const errorSelector = createSelector(
+    [error],
+    (error) => error
 );
