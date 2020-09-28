9b6b44ef700f7c326880da9dce2ce0e1e4c7ad34
GabrielAparicio <sgaparicio22@gmail.com>
2020-09-28 13:45:32 -0500

    Connect UsersList component to the store

--- src/components/UsersList/UsersList.tsx
+++ src/components/UsersList/UsersList.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import UserItem from '../UserItem';
 
-const UsersList = () => {
-    return <div>Users list</div>;
+import LoadingView from '../../views/LoadingView';
+import ErrorView from '../../views/ErrorView';
+import RetryButton from '../RetryButton';
+
+import { usersSelector, isLoadingSelector, errorSelector } from '../../store/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+
+const UsersList: React.FC = () => {
+
+    const users = useSelector(usersSelector);
+    const isLoading = useSelector(isLoadingSelector);
+    const error = useSelector(errorSelector);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch({
+            type: 'FETCH_USERS'
+        });
+    }, [dispatch]);
+
+    if(isLoading)
+        return <LoadingView />;
+
+    if(error) {
+        return (
+            <ErrorView errorMessage='Unable to show users'>
+                <RetryButton />
+            </ErrorView>
+        );
+    }
+        
+    const mappedUsers = users.map((user) => <UserItem key={user.id} userData={user} />);
+    return (
+        <>
+            {mappedUsers}
+        </>
+    );
 };
 
 export default UsersList;
