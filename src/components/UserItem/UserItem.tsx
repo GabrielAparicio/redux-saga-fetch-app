78726756e7bf028847ac1d71e659f8f5b6041f21
GabrielAparicio <sgaparicio22@gmail.com>
2020-09-28 13:45:56 -0500

    Create component to show individual user data

---
+++ src/components/UserItem/UserItem.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { User } from '../../store/types';
+
+import Card from 'react-bootstrap/Card';
+
+interface UserItemProps {
+    userData: User;
+}
+
+const UserItem: React.FC<UserItemProps> = ({ userData }) => {
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant='top' src={userData.cached_avatar} />
+            <Card.Body>
+                <Card.Title>{userData.name}</Card.Title>
+                <Card.Text>
+                {userData.email}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default UserItem;
