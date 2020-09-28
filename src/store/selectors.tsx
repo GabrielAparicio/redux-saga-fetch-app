a631d91fc18f84b17b9fde89a51a76fd5a68907e
GabrielAparicio <sgaparicio22@gmail.com>
2020-09-28 12:02:04 -0500

    Define general users selector

---
+++ src/store/selectors.tsx
@@ -0,0 +1,22 @@
+import { createSelector } from '@reduxjs/toolkit';
+import _ from 'lodash';
+
+import { RootState } from './store';
+
+const users = (state: RootState) => state.usersSlice.users;
+const isLoading = (state: RootState) => state.usersSlice.isLoading;
+const error = (state: RootState) => state.usersSlice.error;
+const sortingField = (state: RootState) => state.sortingSlice.field;
+const sortingType = (state: RootState) => state.sortingSlice.type;
+const filterField = (state: RootState) => state.filterSlice.field;
+const filterContent = (state: RootState) => state.filterSlice.content;
+
+export const usersSelector = createSelector(
+    [users, sortingField, sortingType, filterField, filterContent],
+    (users, sortingField, sortingType, filterField, filterContent) => {
+        const filteredUsers = filterContent ? (
+            users.filter((user) => (user as any)[filterField].toLowerCase().includes(filterContent) )
+        ) : users;
+        return _.orderBy(filteredUsers, [sortingField], [sortingType]);
+    }
+);
